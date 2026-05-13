d2917cb84a8d9385729ce598eba31f8719ba3ea1
joy0789 <johirulislamj62@gmail.comm>
2026-05-13 19:14:01 +0600

    learning naming a variable in a right way

---
+++ naming.js
@@ -0,0 +1,32 @@
+// 1. can't use keyword in variable name
+var false = 45;
+
+// 2. no space or gap in variable name
+var is happy = false;
+
+// right way
+var ishappy = true;
+
+// 3. no quote
+var "address" = "somewhere";
+
+// 4. can't start with a number
+
+var 2money = 100;
+
+// right way
+
+var money2 = 100;
+
+// name is case sensitive
+
+var name = "Joy";
+var Name = "Joy 07";
+
+// how to write a long a variable name
+
+var my current home address = "Dhaka"
+// right way
+var my_current_home_address = "Dhaka" //snake case
+var myCurrentHomeAddress = "Dhaka" // camel case
+var MyCurrentHomeAddress = "Dhaka" // pascal case

dc627b6b83daa19a9e0d2e68cc97fd61c0a817ae
joy0789 <johirulislamj62@gmail.comm>
2026-05-13 18:46:31 +0600

    learning types of variables like string, number, boolean etc..

---
+++ types.js
@@ -0,0 +1,16 @@
+// String
+var name = "Joy 07";
+
+var address = "Dhanmondi 27"
+
+var country = 'Bangladesh';
+
+// Boolean 
+var passed = true;
+var failed = false;
+console.log(passed);
+console.log(typeof passed);
+
+// Number
+var price = 117;
+console.log(typeof price);

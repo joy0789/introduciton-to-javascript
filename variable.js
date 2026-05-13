9584d364dcca3edfb56aaf603af648c78733cdf2
joy0789 <johirulislamj62@gmail.comm>
2026-05-13 18:32:40 +0600

    learning variable

---
+++ variable.js
@@ -0,0 +1,15 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things needed to declare a variable
+*/
+var weight = 60;
+var price = 100;
+var year = 2060;
+var money = 18600;
+
+
+console.log(money);
+
+var ticket = 6;
+ticket= 7;
+console.log(ticket);

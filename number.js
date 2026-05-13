3275b0ad7d4230cc2ad46bcef4ce3cc343da5377
joy0789 <johirulislamj62@gmail.comm>
2026-05-13 19:28:36 +0600

    learning types of number

---
+++ number.js
@@ -0,0 +1,13 @@
+var orangePrice =20;
+var chocolate = 0.5;
+var applePrice = 20;
+
+var totalPrice = orangePrice + applePrice;
+console.log(totalPrice);
+
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
+console.log(orangePrice + applePrice);
+
+// convert a string to number
+var age = parseInt('36');
